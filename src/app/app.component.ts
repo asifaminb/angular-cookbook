3bfce0ad022ff3cf9703c0e4970d1d4b7f7c3e20
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-10-15 12:20:02 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
-}
+export class AppComponent { }
